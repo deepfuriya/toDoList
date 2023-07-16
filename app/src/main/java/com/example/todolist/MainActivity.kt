cccd71a26415d4890344381b1b700e5c1a855148
Deep Furiya <79759607+deepfuriya@users.noreply.github.com>
2023-07-16 14:37:06 -0400

    first test running fine with adding in recylerview

--- app/src/main/java/com/example/todolist/MainActivity.kt
+++ app/src/main/java/com/example/todolist/MainActivity.kt
@@ -10,23 +10,25 @@ import androidx.recyclerview.widget.RecyclerView
 class MainActivity : AppCompatActivity() {
 
     private lateinit var todoAdapter: TodoAdapter
-    private val recylerview = findViewById<RecyclerView>(R.id.recyelerView)
-    private val btnAdd = findViewById<Button>(R.id.btnAddTodo)
+    private lateinit var recylerview: RecyclerView
+    private lateinit var btnAdd: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        btnAdd = findViewById<Button>(R.id.btnAddTodo)
+        recylerview = findViewById<RecyclerView>(R.id.recyelerView)
         todoAdapter = TodoAdapter(mutableListOf())
 
         recylerview.adapter = todoAdapter
         recylerview.layoutManager = LinearLayoutManager(this)
 
         btnAdd.setOnClickListener{
-            val title = findViewById<TextView>(R.id.todoTile).text.toString()
+            val title = findViewById<TextView>(R.id.enterToDoTitle).text.toString()
             val toDo = Todo(title)
             todoAdapter.addToDoList(toDo)
-            findViewById<TextView>(R.id.todoTile).text = ""
+            findViewById<TextView>(R.id.enterToDoTitle).text = ""
         }
 
 
